3dd33541753a9bdf1b6b568bbf98c02c900a1e90
Justin Buchanan <justbuchanan@gmail.com>
2016-12-11 14:08:27 -0800

    fixed up circ2_intersect and added tests sympy geometry is awesome!

--- circ2_intersect.py
+++ circ2_intersect.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python2
 
+from __future__ import print_function
 import numpy as np
 from sympy.solvers import solve
 from sympy import Symbol
+from sympy.geometry import *
 
+# get the intersection points of two circles
+# tiny wrapper over sympy geometry
 def circ2_intersect(p0, r0, p1, r1):
-    x, y = Symbol('x'), Symbol('y')
-    # give the equations of both circles and solve for x and y, giving the two
-    # intersection points as (x,y) pairs
-    s1, s2 = solve([
-        (x - p0[0])**2 + (y-p0[1])**2 - r0**2,
-        (x - p1[0])**2 + (y - p1[1])**2 - r1**2],
-        x, y)
-    return [s1, s2]
+    c1 = Circle(p0, r0)
+    c2 = Circle(p1, r1)
+    ss = intersection(c1, c2)
+    ss = [np.array(s) for s in ss]
+    return ss
 
 
 if __name__ == '__main__':
-    s = circ2_intersect(np.array([0, 0]), 4, np.array([3, 3]), 2)
-    s2 = []
-    for x in s:
-        x = [float(i) for i in x]
-        s2.append(x)
-    s = s2
-
-    print(s)
+    ss = circ2_intersect(np.array([0, 0]), 2, np.array([0, 3]), 2)
+    ss = [np.array(s) for s in ss]
+    print(ss)
---
+++ circ2_intersect_test.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import unittest
+from circ2_intersect import *
+import numpy as np
+import sys
+
+class TestCirc2Intersect(unittest.TestCase):
+
+    def test_two_intersections(self):
+        intersections = circ2_intersect(
+            np.array([0, 0]), 0.7,
+            np.array([0, 1]), 0.7,
+        )
+
+        self.assertEqual(2, len(intersections))
+
+
+    def test_zero_intersections(self):
+        intersections = circ2_intersect(
+            np.array([0, 0]), .1,
+            np.array([10, 10]), .1,
+        )
+
+        print(intersections, file=sys.stderr)
+        self.assertEqual(0, len(intersections))
+
+    def test_one_intersection(self):
+        intersections = circ2_intersect(
+            np.array([0, 0]), 1,
+            np.array([0, 2]), 1,
+        )
+
+        self.assertEqual(1, len(intersections))
