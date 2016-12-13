00b8c1bd8dba6d7a581c55a6c86da5fede46e813
Justin Buchanan <justbuchanan@gmail.com>
2016-12-13 01:47:47 -0800

    control gui works!

--- qt-app/main.py
+++ qt-app/main.py
@@ -1,30 +1,102 @@
 #!/usr/bin/env python2
 import sys
-from PyQt5.QtCore import QObject, QUrl, Qt, QRectF, pyqtProperty, QPointF
+from PyQt5.QtCore import QObject, QUrl, Qt, QRectF, pyqtProperty, QPointF, pyqtSignal
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType
+from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType, QQmlListProperty
 from PyQt5.QtQuick import QQuickPaintedItem
 from PyQt5.QtGui import QColor, QPen, QPainter
 from math import *
 import numpy as np
 
+from arm_driver import *
+from calibration import *
+
 import arm_model
 
-
-
-class RobotView(QQuickPaintedItem):
-    def __init__(self, parent):
+class ArmConfig(QObject):
+    def __init__(self, parent=None):
         QObject.__init__(self, parent)
         self._points = []
-        self._servo0 = pi/2
-        self._servo1 = pi/4
+
+    @pyqtProperty(list)
+    def points(self):
+        return self._points
+    @points.setter
+    def points(self, value):
+        self._points = value
+
+
+class ArmCommand(QObject):
+    def __init__(self, parent=None, values=[]):
+        QObject.__init__(self, parent)
+        self._servos = values
+
+    @pyqtProperty(list)
+    def servos(self):
+        return self._servos
+    @servos.setter
+    def servos(self, value):
+        self._servos = value
+
+
+class ArmDriver(QObject):
+    def __init__(self, parent):
+        QObject.__init__(self, parent)
+        self._command = ArmCommand(None, [100, 100, 100])
+        self._port = '/dev/arduino'
+        self._arm = Arm(port=self.port)
+        self._connectified = self._arm != None
+
+    connectified_changed = pyqtSignal(bool)
+
+    @pyqtProperty(bool, notify=connectified_changed)
+    def connectified(self):
+        return self._connectified
+
+    @pyqtProperty(ArmCommand)
+    def command(self):
+        return self._command
+    @command.setter
+    def command(self, value):
+        self._command = value
+        if value:
+            # print('sent cmd: %s' % value)
+            self._arm.set_servo_values(value.servos)
+
+    @pyqtProperty(str)
+    def port(self):
+        return self._port
+
+
+class ArmModel(QObject):
+
+    config_changed = pyqtSignal(ArmConfig)
+
+    def __init__(self, parent):
+        QObject.__init__(self, parent)
+        self._servo0 = 0
+        self._servo1 = pi/2
+        self._servo2 = pi/4
+        self._config = None
+        self._command = None
 
     # update model points based on servo values
     def recalculate(self):
-        X = np.array([self.servo0, self.servo1])
-        self.points = arm_model.forward(X)
-        self.update()
+        X = np.array([self.servo1, self.servo2])
+        cfg = ArmConfig()
+        cfg.points = arm_model.forward(X)
+        # TODO: show that it's out of bounds?
+        if cfg.points:
+            self.config = cfg
 
+        arm_servos = calc_arm_servos(X)
+        base = calc_base_servo_cmd(self.servo0)
+        servos = [base, arm_servos[0], arm_servos[1]]
+        # TODO: share this code with motion_path.py
+        servos = clip_servos(servos)
+        servos = [int(s) for s in servos]
+        cmd = ArmCommand(None, servos)
+        self.command = cmd
 
     @pyqtProperty(float)
     def servo0(self):
@@ -42,19 +114,55 @@ class RobotView(QQuickPaintedItem):
         self._servo1 = value
         self.recalculate()
 
-    @pyqtProperty(list)
-    def points(self):
-        return self._points
-    @points.setter
-    def points(self, value):
-        self._points = value
+    @pyqtProperty(float)
+    def servo2(self):
+        return self._servo2
+    @servo2.setter
+    def servo2(self, value):
+        self._servo2 = value
+        self.recalculate()
+
+    @pyqtProperty(ArmConfig, notify=config_changed)
+    def config(self):
+        return self._config
+    @config.setter
+    def config(self, value):
+        self._config = value
+
+        self.config_changed.emit(value)
+
+
+    command_changed = pyqtSignal(ArmCommand)
+
+    @pyqtProperty(ArmCommand, notify=command_changed)
+    def command(self):
+        return self._command
+    @command.setter
+    def command(self, value):
+        self._command = value
+        self.command_changed.emit(value)
+
+
+class RobotView(QQuickPaintedItem):
+    def __init__(self, parent):
+        QObject.__init__(self, parent)
+        # self._points = []
+        self._config = None
+
+    # # list of QPointF values
+    @pyqtProperty(ArmConfig)
+    def config(self):
+        return self._config
+    @config.setter
+    def config(self, value):
+        self._config = value
         self.update()
 
     def paint(self, painter):
 
         painter.setRenderHints(QPainter.Antialiasing)
 
-        if not self.points or len(self.points) != 5:
+        if not self.config or len(self.config.points) != 5:
             print("invalid points array, wrong length")
             return
 
@@ -69,19 +177,19 @@ class RobotView(QQuickPaintedItem):
         painter.save()
         painter.setPen(QPen(QColor('blue'), 0.01))
 
+        pts = [QPointF(p[0], p[1]) for p in self.config.points]
+
         for m in arm_model.LINE_MAPPING:
-            p0 = self.points[m[0]]
-            p1 = self.points[m[1]]
-            painter.drawLine(QPointF(p0[0], p0[1]), QPointF(p1[0], p1[1]))
+            p0 = pts[m[0]]
+            p1 = pts[m[1]]
+            painter.drawLine(p0, p1)
         painter.restore()
 
         painter.setPen(QPen(QColor('blue'), 0))
         painter.setBrush(QColor('orange'))
-        for p in self.points:
+        for p in pts:
             rad = 0.01
-            painter.drawEllipse(QPointF(p[0], p[1]), rad, rad)
-
-
+            painter.drawEllipse(p, rad, rad)
 
 
 
@@ -89,11 +197,14 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     qmlRegisterType(RobotView, 'RobotView', 1, 0, 'RobotView')
+    qmlRegisterType(ArmDriver, 'ArmDriver', 1, 0, 'ArmDriver')
+    qmlRegisterType(ArmModel, 'ArmModel', 1, 0, 'ArmModel')
+    qmlRegisterType(ArmConfig, 'ArmConfig', 1, 0, 'ArmConfig')
+    qmlRegisterType(ArmCommand, 'ArmCommand', 1, 0, 'ArmCommand')
 
     engine = QQmlApplicationEngine()
     ctx = engine.rootContext()
     ctx.setContextProperty("main", engine)
-
 
     engine.load('main.qml')
 
@@ -105,15 +216,15 @@ if __name__ == "__main__":
     # button = win.findChild(QObject, "myButton")
     # button.clicked.connect(myHandler)
 
-    rview = win.findChild(RobotView, "robot")
+    # rview = win.findChild(RobotView, "robot")
 
-    servo0slider = win.findChild(QObject, "servo0")
-    servo1slider = win.findChild(QObject, "servo1")
+    # servo0slider = win.findChild(QObject, "servo0")
+    # servo1slider = win.findChild(QObject, "servo1")
 
-    def recalculate():
-        # X = np.array([pi/2, pi/4])
-        X = np.array([servo0slider.value, servo1slider.value])
-        rview.points = arm_model.forward(X)
+    # def recalculate():
+    #     # X = np.array([pi/2, pi/4])
+    #     X = np.array([servo0slider.value, servo1slider.value])
+    #     rview.points = arm_model.forward(X)
 
     # rview.servo0.valueChanged.connect(lambda: recalculate())
 
--- qt-app/main.qml
+++ qt-app/main.qml
@@ -4,12 +4,31 @@ import QtQuick.Controls 1.2
 import QtQuick.Dialogs 1.1
 import RobotView 1.0
 import QtQuick.Layouts 1.2
+import ArmDriver 1.0
+import ArmCommand 1.0
+import ArmModel 1.0
 
 ApplicationWindow {
     title: qsTr("Test Invoke")
 
     width: 200
     height: 200
+
+    ArmModel {
+        id: arm
+        objectName: "arm"
+    }
+
+    ArmDriver {
+        id: armDriver
+        objectName: "armDriver"
+    }
+
+    Binding {
+        target: armDriver
+        property: "command"
+        value: arm.command
+    }
 
     ColumnLayout {
         id: column
@@ -20,72 +39,97 @@ ApplicationWindow {
         anchors.right: parent.right
         anchors.margins: column.spacing
 
+
         GroupBox {
             title: "Drawing"
 
             Layout.fillWidth: true
 
             RobotView {
-                id: robot
-                // x: 0
-                // y: 0
+                id: robotDrawing
+                objectName: "robotDrawing"
                 // anchors.fill: parent
-                objectName: "robot"
-                // width: parent.parent.width
-                width: 500
-                height: 500
-                // anchors.fill: parent
-                anchors.centerIn: parent
+                // anchors.centerIn: parent
+                width: 200
+                height: 200
             }
         }
 
+        GroupBox {
+            title: "Arduino"
+
+            ColumnLayout {
+                anchors.fill: parent
+                Text {
+                    objectName: "portLabel"
+                    text: "Port: " + armDriver.port
+                }
+                CheckBox {
+                    text: "connected"
+                    checked: armDriver.connectified
+                }
+            }
+        }
         // Button{
         //     objectName: "myButton"
         //     text : "About"
         //     y : 70
         // }
         GroupBox {
-            title: "Servo 0"
+            title: "Servos"
 
+            ColumnLayout {
+                anchors.fill: parent
 
-            Slider {
-                id: servo0
-                objectName: "servo0"
+                Slider {
+                    id: servo0
+                    objectName: "servo0"
 
-                // y: 100
+                    minimumValue: -3.14/2;
+                    maximumValue: 3.14/2
+                    value: 0.2
+                }
 
-                minimumValue: 0
-                maximumValue: 3.14
-                value: 1.8
-                // height: 50
-            }
-        }
+                Slider {
+                    id: servo1
+                    objectName: "servo1"
 
-        GroupBox {
-            title: "Servo 1"
-            Slider {
-                id: servo1
-                objectName: "servo0"
+                    minimumValue: 0
+                    maximumValue: 3.14
+                    value: 1.8
+                }
 
-                // y: 150
+                Slider {
+                    id: servo2
+                    objectName: "servo2"
 
-                minimumValue: 0
-                maximumValue: 3.14
-                value: 0.2
-                // height: 50
+                    minimumValue: 0
+                    maximumValue: 3.14
+                    value: 0.2
+                }
             }
         }
 
     }
 
     Binding {
-        target: robot
+        target: arm
         property: "servo0"
         value: servo0.value
     }
     Binding {
-        target: robot
+        target: arm
         property: "servo1"
         value: servo1.value
+    }
+    Binding {
+        target: arm
+        property: "servo2"
+        value: servo2.value
+    }
+    Binding {
+        target: robotDrawing
+        property: "config"
+        value: arm.config
     }
 }
